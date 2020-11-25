203634d5f51461456483d7d785bec28b0e121d7b
James HP <naurotj@gmail.com>
2020-11-25 12:26:51 -0500

    Added div to SimpleCalculator

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,9 +1,11 @@
 package travis_ci_tutorial_java;
 
+
 public class SimpleCalculator {
 	public int add(int a, int b) {
 		return a + b;
 	}
 	public int minus(int a, int b) { return a - b; }
 	public int mult(int a, int b) { return a * b; }
+	public int div(int a, int b) { return  a / b; }
 }
